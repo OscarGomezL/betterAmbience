3ccc51d5e6913678fe2826c633b3de64e68f9c94
Oscar Gomez <oscarthedev@gmail.com>
2022-03-09 23:44:31 -0500

    [ADD] header component & styles

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header>
+			<img src="https://i.imgur.com/CiSmn0s.png" alt="A" />
+			<div className="header-title">betterAmbience</div>
+		</header>
+	)
+}
