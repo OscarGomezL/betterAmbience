4a8640e82a2231f3ce769be26ff94addb98e217e
Oscar Gomez <oscarthedev@gmail.com>
2022-03-14 00:00:45 -0500

    [ADD] soundRange component & styles

--- components/SoundRange.jsx
+++ components/SoundRange.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react"
+import { toggleSize } from "../logic/functions"
+
 export default function SoundRange() {
+	const [range, setRange] = useState(5)
 	return (
-		<div>soundRange</div>
+		<div className="sound_range">
+			<input 
+				type="range"
+				min={0}
+				max={10}
+				value={range}
+				onChange={e=>{setRange(e.target.value)}}
+			/>
+			{toggleSize(range)}
+		</div>
 	)
 }
