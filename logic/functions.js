8a59f3bcd2c9bdf1ff50cded8890828a8cc1f9c3
Oscar Gomez <oscarthedev@gmail.com>
2022-03-14 00:00:10 -0500

    [ADD] necessary imports in functions js

--- logic/functions.js
+++ logic/functions.js
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faVolumeOff, faVolumeUp, faVolumeDown} from '@fortawesome/free-solid-svg-icons'
+
+
 export function toggleClass(num) {
 	let selectedDiv = document.querySelector(`.rainbow${num}`)
 	selectedDiv.classList.toggle('rainbow')
 }
 export function toggleSize (num) {
-	if(num==1) return <FontAwesomeIcon className="speaker1 speaker" icon={faVolumeOff}></FontAwesomeIcon>
-	if(num==2) return <FontAwesomeIcon className="speaker2 speaker" icon={faVolumeDown}></FontAwesomeIcon>
-	if(num==3) return <FontAwesomeIcon className="speaker3 speaker" icon={faVolumeUp}></FontAwesomeIcon>
+	if(num<=3) return <FontAwesomeIcon className="speaker1 speaker" icon={faVolumeOff}></FontAwesomeIcon>
+	if(num<=6) return <FontAwesomeIcon className="speaker2 speaker" icon={faVolumeDown}></FontAwesomeIcon>
+	if(num<=10) return <FontAwesomeIcon className="speaker3 speaker" icon={faVolumeUp}></FontAwesomeIcon>
 }
