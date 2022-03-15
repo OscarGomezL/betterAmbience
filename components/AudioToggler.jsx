aa45851a429b7bbb3306225d75b498f8e452ab1a
Oscar Gomez <oscarthedev@gmail.com>
2022-03-15 18:51:25 -0500

    [ADD] getIcon function in audioToggler component, now each component displays icons

--- components/AudioToggler.jsx
+++ components/AudioToggler.jsx
@@ -1,14 +1,12 @@
 import SoundRange from './SoundRange'
-import { toggleClass } from '../logic/functions'
-
+import { getIcon, toggleClass } from '../logic/functions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function AudioToggler({index}) {
 	return (
 		<div className={`audioToggler rainbow${index}`}>
 			<div className="audioToggler-container">
 				<div className="audioToggler-icon_container" onClick={()=>{toggleClass(index)}}>
-					<div className="audioToggler-icon">
-						AudioToggler {index}
-					</div>
+					<FontAwesomeIcon icon={getIcon(index-1)} className="audioToggler_icon"></FontAwesomeIcon>
 				</div>
 				<SoundRange/>
 			</div>
