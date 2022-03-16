3fcb267a69ca564f15a8267924b2d513d9acba95
Oscar Gomez <oscarthedev@gmail.com>
2022-03-15 20:14:10 -0500

    [ADD] now AudioToggler can play a sound

--- components/AudioToggler.jsx
+++ components/AudioToggler.jsx
@@ -1,14 +1,30 @@
 import SoundRange from './SoundRange'
+import { useState } from 'react'
 import { getIcon, toggleClass } from '../logic/functions'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useSound from 'use-sound'
+import { returnFileName } from '../logic/functions'
+
 export default function AudioToggler({index}) {
+	const [isPlaying, setIsPlaying] = useState(true)
+	const [volume, setVolume] = useState(0.5)
+    const [play, {stop}] = useSound(`/sounds/${returnFileName(index)}.mp3`,{volume, loop: true})
+	const soundToggler = (index) => {
+		isPlaying ? play() : stop();
+		setIsPlaying(!isPlaying)
+		toggleClass(index)
+	}
 	return (
 		<div className={`audioToggler rainbow${index}`}>
 			<div className="audioToggler-container">
-				<div className="audioToggler-icon_container" onClick={()=>{toggleClass(index)}}>
+				<div 
+					className="audioToggler-icon_container"
+					onClick={()=>{soundToggler(index)}}
+					title={returnFileName(index)}
+				>
 					<FontAwesomeIcon icon={getIcon(index-1)} className="audioToggler_icon"></FontAwesomeIcon>
 				</div>
-				<SoundRange/>
+				<SoundRange changeVolume={setVolume}/>
 			</div>
 		</div>
 	)
