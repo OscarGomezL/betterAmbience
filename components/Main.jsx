69fcbde20256d1821ebfc2c379f9026da69ec02b
Oscar Gomez <oscarthedev@gmail.com>
2022-03-15 20:15:45 -0500

    [PATCH] now main calls 12 AudioToggler not 10

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,7 +2,7 @@ import AudioToggler from "./AudioToggler"
 
 export default function Main() {
 	let arr = []
-	for(let i = 1; i<=10; i++) arr.push(i)
+	for(let i = 1; i<=12; i++) arr.push(i)
 	return (
 		<main>
 			{arr.map(num=><AudioToggler key={num} index={num}/>)}
