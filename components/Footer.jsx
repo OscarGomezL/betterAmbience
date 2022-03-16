07e5bd02e58ae1311ed0fdb812aa2bb4250c6c33
Oscar Gomez <oscarthedev@gmail.com>
2022-03-15 21:41:08 -0500

    [ADD] github link to footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,9 @@
 export default function Footer() {
 	return (
-		<footer></footer>
+		<footer>
+			<a href="https://github.com/OscarGomezL" target="_blank" className='footer-github footer-icon'>
+				<img src="/github.png"/>
+			</a>
+		</footer>
 	)
 }
