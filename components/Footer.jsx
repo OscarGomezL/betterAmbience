1c1433a2fba8e1783443ac220a4d715f65a02738
Oscar Gomez <oscarthedev@gmail.com>
2022-03-16 19:48:36 -0500

    [ADD] rel="noreferrer"

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer>
-			<a href="https://github.com/OscarGomezL" target="_blank" className='footer-github footer-icon'>
+			<a href="https://github.com/OscarGomezL" target="_blank" rel="noreferrer" className='footer-github footer-icon'>
 				<img src="/github.png"/>
 			</a>
 		</footer>
