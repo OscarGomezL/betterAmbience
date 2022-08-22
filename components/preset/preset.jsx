71976391eb5a7088de691b0b0bd87cb9c7c37047
Oscar Gomez <oscarthedev@gmail.com>
2022-08-21 21:59:16 -0600

    [ADD] "preset" & "presets" component

---
+++ components/preset/preset.jsx
@@ -0,0 +1,7 @@
+export default function Preset() {
+	return (
+		<div className="preset">
+			
+		</div>
+	)
+}
---
+++ components/preset/presets.jsx
@@ -0,0 +1,7 @@
+export default function Presets() {
+	return (
+		<section className="preset_container">
+			
+		</section>
+	)
+}
