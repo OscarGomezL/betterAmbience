50907d7810e6b3f8a76a7344cebf0148581a1377
Oscar Gomez <oscarthedev@gmail.com>
2022-08-21 21:25:44 -0600

    [DELETE] content from footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,6 @@
 export default function Footer() {
 	return (
 		<footer>
-			<a href="https://github.com/OscarGomezL" target="_blank" rel="noreferrer" className='footer-github footer-icon'>
-				<img src="/github.png"/>
-			</a>
 		</footer>
 	)
 }
