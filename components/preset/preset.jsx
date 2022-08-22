0b202a4b2edad6873f0f06ef1ed651745adca2e7
Oscar Gomez <oscarthedev@gmail.com>
2022-08-22 14:53:00 -0600

    [ADD] 'preset' component now clicks the audioToggler onClick

--- components/preset/preset.jsx
+++ components/preset/preset.jsx
@@ -1,7 +1,15 @@
-export default function Preset() {
+export default function Preset({info}) {
+	function toggle() {
+		for(let i=0; i<info.sounds.length; i++) {
+			document.querySelector(`.toggler_${info.sounds[i].soundIndex}`).click()
+		}
+	}
 	return (
-		<div className="preset">
-			
+		<div 
+			className="preset"
+			onClick={toggle}
+		>
+			{info.name}
 		</div>
 	)
 }
