c982648db2fc447df57b3aeefd921b3fa43f36a0
Oscar Gomez <oscarthedev@gmail.com>
2022-08-21 21:25:56 -0600

    [DELETE] content from header

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,6 @@
 export default function Header() {
 	return (
 		<header>
-			<img src="https://i.imgur.com/CiSmn0s.png" alt="betterAmbience" draggable="false"/>
-			<div className="header-title">betterAmbience</div>
 		</header>
 	)
 }
