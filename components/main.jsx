2dd5f2f5141a94b3d359db6518ceba7fa469c0f7
Oscar Gomez <oscarthedev@gmail.com>
2022-08-21 21:59:31 -0600

    [ADD] import preset css and jsx

--- components/main.jsx
+++ components/main.jsx
@@ -1,11 +1,12 @@
 import AudioToggler from "./audioToggler"
+import Presets from "./preset/presets"
 
 export default function Main() {
 	let arr = []
 	for(let i = 1; i<=12; i++) arr.push(i)
 	return (
 		<main>
-			<div></div>
+			<Presets/>
 			{arr.map(num=><AudioToggler key={num} index={num}/>)}
 		</main>
 	)
--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import '../styles/main.css'
 import '../styles/header.css'
 import '../styles/audioToggler.css'
 import '../styles/soundRange.css'
+import '../styles/presets.css'
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
